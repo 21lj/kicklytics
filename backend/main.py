f550c972e1623d9e2990de382ac1d7f62d67984f
Lijo Joseph <lijojoseph0021@gmail.com>
2026-07-23 12:41:46 +0530

    feature: tracks player with ball acquistion by annote a red triangle on top

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 from utils.video_utils import get_video, save_video
 from features.tracker import Tracker
 from features.team_assigner import TeamAssigner
+from features.player_ball_assigner import PlayerBallAssigner
 
 def main():
     video_frames = get_video('./input_videos/test1.mp4')
@@ -17,6 +18,14 @@ def main():
             team = team_assigner.get_player_team(video_frames[frame_no], track['bbox'], player_id)
             tracks['players'][frame_no][player_id]['team'] = team
             tracks['players'][frame_no][player_id]['team_color'] = team_assigner.team_color[team]
+
+    player_assigner = PlayerBallAssigner()
+    for frame_no, player_track in enumerate(tracks['players']):
+        ball_bbox = tracks['ball'][frame_no][1]['bbox']
+        assigned_player = player_assigner.assign_ball_to_player(player_track, ball_bbox)
+
+        if assigned_player != -1:
+            tracks['players'][frame_no][assigned_player]['has_ball'] = True
     
     outframe = tracker.draw_annotations(video_frames, tracks)
     save_video(outframe, './output_videos/out.avi')
